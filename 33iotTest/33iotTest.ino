9708a3b6dfe01b7195ae1d720ffb96fd5912be40
ProfessorX404 <xseggs@gmail.com>
2023-01-21 16:18:19 -0800

    Schematic rev c

--- 33iotTest/33iotTest.ino
+++ 33iotTest/33iotTest.ino
@@ -9,9 +9,11 @@ void setup() {
 
     // Enable the port multiplexer for pins D7
     PORT->Group[g_APinDescription[2].ulPort].PINCFG[g_APinDescription[2].ulPin].bit.PMUXEN = 1;
+    PORT->Group[g_APinDescription[6].ulPort].PINCFG[g_APinDescription[6].ulPin].bit.PMUXEN = 1;
 
     // D7 is on EVEN port pin PA06 and TCC1/WO[0] channel 0 is on peripheral E
     PORT->Group[g_APinDescription[2].ulPort].PMUX[g_APinDescription[2].ulPin >> 1].reg = /*PORT_PMUX_PMUXO_E |*/ PORT_PMUX_PMUXE_F;
+    PORT->Group[g_APinDescription[6].ulPort].PMUX[g_APinDescription[6].ulPin >> 1].reg = /*PORT_PMUX_PMUXO_E |*/ PORT_PMUX_PMUXE_E;
 
     // Normal (single slope) PWM operation: timer countinuouslys count up to PER register value and then is reset to 0
     TCC0->WAVE.reg |= TCC_WAVE_WAVEGEN_NPWM; // Setup single slope PWM on TCC1
@@ -20,10 +22,6 @@ void setup() {
 
     TCC0->PER.reg = 1262; // Set the frequency of the PWM on TCC1 to 38kHz: 48MHz / (1262 + 1) = 38kHz
     while (TCC0->SYNCBUSY.bit.PER)
-        ; // Wait for synchronization
-
-    TCC0->CC[0].reg = 315; // TCC1 CC0 - 50% duty cycle on D7
-    while (TCC0->SYNCBUSY.bit.CC0)
         ; // Wait for synchronization
 
     TCC0->CTRLA.bit.ENABLE = 1; // Enable the TCC1 counter
@@ -36,13 +34,19 @@ void setup() {
 void loop() {
     digitalWrite(LED_BUILTIN, LOW);
     // Using buffered counter compare registers (CCBx)
-    TCC0->CCB[0].reg = 2; // TCC1 CCB1 - 25% duty cycle on D7
+    TCC0->CCB[0].reg = 0; // TCC1 CCB1 - 25% duty cycle on D2
     while (TCC0->SYNCBUSY.bit.CCB0)
+        ;                    // Wait for synchronization
+    TCC0->CCB[1].reg = 1261; // TCC1 CCB1 - 25% duty cycle on D6
+    while (TCC0->SYNCBUSY.bit.CCB1)
         ;        // Wait for synchronization
     delay(1000); // Wait for 1 second
     digitalWrite(LED_BUILTIN, HIGH);
-    TCC0->CCB[0].reg = 1261; // TCC1 CCB1 - 75% duty cycle on D7
+    TCC0->CCB[0].reg = 1261; // TCC1 CCB1 - 75% duty cycle on D2
     while (TCC0->SYNCBUSY.bit.CCB0)
+        ;                 // Wait for synchronization
+    TCC0->CCB[1].reg = 0; // TCC1 CCB1 - 75% duty cycle on D6
+    while (TCC0->SYNCBUSY.bit.CCB1)
         ;        // Wait for synchronization
     delay(1000); // Wait for 1 second
 }
